db421953777fc4802ad542a107ee7bb0e7d1c8d6
binsmyth <binsmyth@gmail.com>
2019-09-17 13:13:40 +1000

    Signed-off-by: binsmyth <binsmyth@gmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';//thunk
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react'
+
 import ProductReducer from './ProductReducer';
 import AppNavigator from './AppNavigator';
-import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, ProductReducer);
 
 const store = createStore(
-    ProductReducer, 
+    persistedReducer, 
     applyMiddleware(thunk)
   );
+
+let persistor = persistStore(store);
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={ store }>
-        <AppNavigator />
+         <PersistGate loading={null} persistor={persistor}>
+          <AppNavigator />
+        </PersistGate>
       </Provider>
     );
   }
